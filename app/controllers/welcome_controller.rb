bcc681b00b99bda9009c8a1dc7f036ccee749f34
Hiroyuki Kondo <hiroyuki.kondo@interfirm.co.jp>
2010-07-26 16:44:17 +0900

    will_paginate applied

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    # @posts = Posts.paginate(:all, 
+    #                         :order => "updated_at DESC", 
+    #                         :conditions => 'models.deleted_at IS NULL',
+    #                         :per_page => 10, 
+    #                         :page => params[:page])
   end
 
 end
